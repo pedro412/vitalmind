b7bb03675638ee2662add324ced91b79c099d688
pedro412 <pedro.alvarez@datagran.io>
2020-06-18 12:33:02 -0500

    [ADD] Slider function

---
+++ public/js/slider.js
@@ -0,0 +1,35 @@
+"use strict";
+const image = document.getElementById("pasosImage");
+const pasosNode = document.getElementsByClassName("pasos");
+const activeBall = document.createElement("div");
+let currentStep = 1;
+
+const createSteps = () => {
+  Array.from(pasosNode[0].children).forEach((element, index) => {
+    element.setAttribute("step", index + 1);
+    element.addEventListener("mouseenter", () => {
+      currentStep = element.getAttribute("step");
+      setActiveToCurrentStep();
+      getImageForStep();
+    });
+  });
+};
+
+const setActiveToCurrentStep = () => {
+  Array.from(pasosNode[0].children).forEach((element, index) => {
+    if (index + 1 == currentStep) {
+      element.classList.add("active");
+      element.appendChild(activeBall);
+    } else {
+      element.classList.remove("active");
+    }
+  });
+};
+
+const getImageForStep = () => {
+  image.src = `./assets/step${currentStep}.png`;
+};
+
+createSteps();
+setActiveToCurrentStep();
+getImageForStep();
